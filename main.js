2a7f8442a5c93be2c3cc9182d226deedbb5d269d
sd025 <shubham.dhumal452@gmail.com>
2024-05-23 22:12:01 +0530

    filter poly

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-Array.prototype.myMap = function (cb) {
-  let temp = [];
-  for (let i = 0; i < this.length; i++) { 
-    temp. push(cb(this[i], i, this));
+// map
+// Array.prototype.myMap = function (cb) {
+//   let temp = [];
+//   for (let i = 0; i < this.length; i++) { 
+//     temp. push(cb(this[i], i, this));
+//   }
+//   return temp;
+//   }
+//   const nums = [1, 2, 3, 41];
+//   const multiplyThree = nums.myMap((num, i, arr) => {
+//   return num * 3;
+//   })
+//   console.log(multiplyThree);
+
+
+  // filter
+  Array. prototype.myFilter = function (cb) {
+    let temp = [];
+    for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) temp.push(this[i]);
   }
-  return temp;
+    return temp;
   }
-  const nums = [1, 2, 3, 41];
-  const multiplyThree = nums.myMap((num, i, arr) => {
-  return num * 3;
+    const nums = [1, 2, 3, 4];
+    const moreThanTwo = nums.filter ((num) => {
+    return num > 2;
   })
-  console.log(multiplyThree);
+  console.log(moreThanTwo);
