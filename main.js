41ecc8d378aa7d61ba09916d081e1e1ccea6491c
sd025 <shubham.dhumal452@gmail.com>
2024-05-23 22:13:57 +0530

    filter

--- main.js
+++ main.js
@@ -17,8 +17,8 @@
   Array. prototype.myFilter = function (cb) {
     let temp = [];
     for (let i = 0; i < this.length; i++) {
-    if (cb(this[i], i, this)) temp.push(this[i]);
-  }
+      if (cb(this[i], i, this)) temp.push(this[i]);
+    }
     return temp;
   }
     const nums = [1, 2, 3, 4];
