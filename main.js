241275581889c293dfcecd6574f155993e5877dc
sd025 <shubham.dhumal452@gmail.com>
2024-05-23 22:09:11 +0530

    map Polyfill

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+Array.prototype.myMap = function (cb) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) { 
+    temp. push(cb(this[i], i, this));
+  }
+  return temp;
+  }
+  const nums = [1, 2, 3, 41];
+  const multiplyThree = nums.myMap((num, i, arr) => {
+  return num * 3;
+  })
+  console.log(multiplyThree);
